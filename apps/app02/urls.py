7e3b236f413f72dc8784f9e5668928ad142bbb99
yuanyuan <xu.yuanyuan@bekaert.com>
2024-10-21 18:10:01 +0800

    add gitignore

--- apps/app02/urls.py
+++ apps/app02/urls.py
@@ -2,10 +2,10 @@ from fastapi import APIRouter
 
 shop = APIRouter()
 
-@shop.food("/shop/food")
+@shop.get("/shop/food")
 def user_login():
     return {"shop": "food"}
 
-@shop.bed("/shop/bed")
+@shop.get("/shop/bed")
 def user_register():
     return {"shop": "bed"}
--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI
 import uvicorn
-from apps.app01.urls import shop
-from apps.app02.urls import user
+from apps.app01.urls import user
+from apps.app02.urls import shop
 
 app = FastAPI()
 
 app.include_router(shop, 
-                   prefix="shop", 
+                   prefix="/shop", 
                    tags=["shop info"])
 
 app.include_router(user, 
-                   prefix="user", 
+                   prefix="/user", 
                    tags=["user info"])
 
 if __name__ == "__main__":
-    uvicorn.run(app=app, host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run(app=app, host="0.0.0.0", port=8000, reload=True)
